a9c523b92ed9fe383c5a86627ca096b4646fff6e
Naomi Jouan <naomi.jouan@umontreal.ca>
2025-11-19 13:52:25 -0500

    Update mode-switch.js

--- mode-switch.js
+++ mode-switch.js
@@ -50,7 +50,7 @@ function setHappyMode() {
   const page1 = document.getElementById("resumePage1");
   const page2 = document.getElementById("resumePage2");
 
-  if (resumeLink) resumeLink.href = "assets/NaomiJouan_CV2_v2.pdf";
+  if (resumeLink) resumeLink.href = "assets/NaomiJouan_CV.pdf";
   if (page1) page1.src = "assets/CV2Page1_v2.png";
   if (page2) page2.src = "assets/CV2Page2_v2.png";
 
